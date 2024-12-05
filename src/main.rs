68e4b8e74d4ba89c2a3d80efbc5c62350c7ecffd
Raphael Höser <raphael@hoeser.dev>
2024-12-05 16:11:30 +0100

    Add execution path for running all solutions

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,22 @@ pub mod days;
 pub mod solution;
 pub mod utils;
 
-use solution::Solution;
 use days::*;
+use solution::{Part, Solution};
 
 #[derive(Parser, Debug)]
 struct Args {
     /// Name of the person to greet
     #[arg(short, long)]
-    day: usize,
+    day: Option<usize>,
 
     /// Number of times to greet
     #[arg(short, long, default_value_t = false)]
     example: bool,
 }
 
-fn main() {
-    let args = Args::parse();
-    let day = args.day;
-    let example = args.example;
-    let solution: Option<Box<dyn Solution>> = match day {
+fn get_day(day: usize) -> Option<Box<dyn Solution>> {
+    match day {
         0 => Some(Box::new(day00::Day)),
         1 => Some(Box::new(day01::Day)),
         2 => Some(Box::new(day02::Day)),
@@ -29,34 +26,47 @@ fn main() {
         4 => Some(Box::new(day04::Day)),
         5 => Some(Box::new(day05::Day)),
         _ => None,
-    };
-    if let Some(solution) = solution {
-        println!("Day {}:", day);
-        if let Some(input) = utils::read_input(day, example, 1) {
+    }
+}
+
+fn run_part(day: usize, part: Part, example: bool) {
+    let input = utils::read_input(day, example, part.into());
+    if let Some(solution) = get_day(day) {
+        if let Some(input) = input {
             let start = std::time::Instant::now();
-            let result = solution.part1(&input);
+            let result = solution.run(&input, part);
             let duration = start.elapsed();
             if let Some(result) = result {
-                println!("Part 1: {} (took {:?})", result, duration);
+                println!("Part {}: {} - {:?}", part, result, duration);
             } else {
-                println!("Part 1: not implemented for day {day}");
+                println!("Part {}: not implemented for day {day}", part);
             }
         } else {
-            println!("Part 1: no input found for day {day} (example: {example})");
-        }
-        if let Some(input) = utils::read_input(day, example, 2) {
-            let start = std::time::Instant::now();
-            let result = solution.part2(&input);
-            let duration = start.elapsed();
-            if let Some(result) = result {
-                println!("Part 2: {} (took {:?})", result, duration);
-            } else {
-                println!("Part 2: not implemented for day {day}");
-            }
-        } else {
-            println!("Part 2: no input found for day {day} (example: {example})");
+            println!(
+                "Part {}: no input found for day {day} (example: {example})",
+                part
+            );
         }
     } else {
         println!("Day {} not implemented", day);
+    }
+}
+
+fn run_day(day: usize, example: bool) {
+    println!("Day {}:", day);
+    run_part(day, Part::One, example);
+    run_part(day, Part::Two, example);
+}
+
+fn main() {
+    let args = Args::parse();
+    if let Some(day) = args.day {
+        run_day(day, args.example);
+    } else {
+        for day in 1..25 {
+            if get_day(day).is_some() {
+                run_day(day, args.example);
+            }
+        }
     }
 }
--- src/solution.rs
+++ src/solution.rs
@@ -1,8 +1,41 @@
+use std::fmt::Display;
+
 pub trait Solution {
     fn part1(&self, _input: &str) -> Option<usize> {
         None
     }
     fn part2(&self, _input: &str) -> Option<usize> {
         None
+    }
+
+    fn run(&self, input: &str, part: Part) -> Option<usize> {
+        match part {
+            Part::One => self.part1(input),
+            Part::Two => self.part2(input),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Part {
+    One,
+    Two,
+}
+
+impl Display for Part {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Part::One => write!(f, "1"),
+            Part::Two => write!(f, "2"),
+        }
+    }
+}
+
+impl From<Part> for u8 {
+    fn from(part: Part) -> u8 {
+        match part {
+            Part::One => 1,
+            Part::Two => 2,
+        }
     }
 }
