75c7c7998280ac43b416389bdad39fbb643d4fe2
Raphael Höser <raphael@hoeser.info>
2024-12-08 11:59:58 +0100

    Add day 8.2
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::{HashMap, HashSet},
+    ops::{Add, Sub},
     str::FromStr,
 };
 
@@ -7,8 +8,30 @@ use crate::solution::Solution;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Position {
-    col: usize,
-    row: usize,
+    col: isize,
+    row: isize,
+}
+
+impl Add<(isize, isize)> for Position {
+    type Output = Self;
+
+    fn add(self, other: (isize, isize)) -> Self {
+        Position {
+            col: self.col + other.1,
+            row: self.row + other.0,
+        }
+    }
+}
+
+impl Sub<(isize, isize)> for Position {
+    type Output = Self;
+
+    fn sub(self, other: (isize, isize)) -> Self {
+        Position {
+            col: self.col - other.1,
+            row: self.row - other.0,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -19,15 +42,15 @@ pub struct AntennaGrid {
 }
 
 impl AntennaGrid {
-    fn get_antinodes(&self) -> HashSet<Position> {
+    fn get_antinodes(&self, resonants: bool) -> HashSet<Position> {
         let mut antinodes = HashSet::new();
-        for (_, positions) in &self.antennas {
+        for positions in self.antennas.values() {
             for i in 0..positions.len() {
                 for j in i + 1..positions.len() {
                     let self_pos = positions[i];
                     let other_pos = positions[j];
 
-                    for antinode in self.get_pos_antinodes(self_pos, other_pos) {
+                    for antinode in self.get_pos_antinodes(self_pos, other_pos, resonants) {
                         antinodes.insert(antinode);
                     }
                 }
@@ -36,29 +59,34 @@ impl AntennaGrid {
         antinodes
     }
 
-    fn pos_in_bounds(&self, row: isize, col: isize) -> bool {
-        (0..self.width as isize).contains(&col) && (0..self.height as isize).contains(&row)
+    fn pos_in_bounds(&self, pos: Position) -> bool {
+        (0..self.width as isize).contains(&pos.col) && (0..self.height as isize).contains(&pos.row)
     }
 
-    fn get_pos_antinodes(&self, a: Position, b: Position) -> Vec<Position> {
+    fn get_pos_antinodes(&self, a: Position, b: Position, resonants: bool) -> Vec<Position> {
         let mut antinodes = Vec::new();
-        let dx = b.col as isize - a.col as isize;
-        let dy = b.row as isize - a.row as isize;
-        let na = (a.row as isize - dy, a.col as isize - dx);
-        if self.pos_in_bounds(na.0, na.1) {
-            let na = Position {
-                col: na.1 as usize,
-                row: na.0 as usize,
-            };
+        if resonants {
+            antinodes.push(a);
+            antinodes.push(b);
+        }
+        let vector = (b.row - a.row, b.col - a.col);
+        let mut na = a - vector;
+        if self.pos_in_bounds(na) {
             antinodes.push(na);
         }
-        let nb = (b.row as isize + dy, b.col as isize + dx);
-        if self.pos_in_bounds(nb.0, nb.1) {
-            let nb = Position {
-                col: nb.1 as usize,
-                row: nb.0 as usize,
-            };
+        let mut nb = b + vector;
+        if self.pos_in_bounds(nb) {
             antinodes.push(nb);
+        }
+        if resonants {
+            while self.pos_in_bounds(na) {
+                antinodes.push(na);
+                na = na - vector;
+            }
+            while self.pos_in_bounds(nb) {
+                antinodes.push(nb);
+                nb = nb + vector;
+            }
         }
         antinodes
     }
@@ -73,10 +101,10 @@ impl FromStr for AntennaGrid {
         for (row, line) in lines.clone().enumerate() {
             for (col, c) in line.chars().enumerate() {
                 if c != '.' {
-                    antennas
-                        .entry(c)
-                        .or_insert_with(Vec::new)
-                        .push(Position { col, row });
+                    antennas.entry(c).or_insert_with(Vec::new).push(Position {
+                        col: col as isize,
+                        row: row as isize,
+                    });
                 }
             }
         }
@@ -93,11 +121,12 @@ pub struct Day;
 impl Solution for Day {
     fn part1(&self, input: &str) -> Option<usize> {
         let grid: AntennaGrid = input.parse().unwrap();
-        Some(grid.get_antinodes().len())
+        Some(grid.get_antinodes(false).len())
     }
 
-    fn part2(&self, _input: &str) -> Option<usize> {
-        None
+    fn part2(&self, input: &str) -> Option<usize> {
+        let grid: AntennaGrid = input.parse().unwrap();
+        Some(grid.get_antinodes(true).len())
     }
 }
 
@@ -123,11 +152,11 @@ mod tests {
     #[test]
     fn test_part2_example() {
         let input = read_input(DAY, true, 2).unwrap();
-        assert_eq!(Day.part2(&input), None);
+        assert_eq!(Day.part2(&input), Some(34));
     }
     #[test]
     fn test_part2_challenge() {
         let input = read_input(DAY, false, 2).unwrap();
-        assert_eq!(Day.part2(&input), None);
+        assert_eq!(Day.part2(&input), Some(949));
     }
 }
