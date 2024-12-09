3286ef1913ed516e93aadfe037da0d3a0af23b31
Raphael Höser <raphael.hoeser@aptiv.com>
2024-12-09 09:57:28 +0100

    Add impl for part 2

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -60,6 +60,66 @@ fn defrag(storage: &mut Vec<Option<usize>>) -> Vec<usize> {
     defragged
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum Block {
+    Used(usize, usize),
+    Free(usize),
+}
+
+fn merge_blocks(used: &[usize], available: &[usize]) -> Vec<Option<usize>> {
+    let mut block_list = Vec::new();
+
+    for i in 0..used.len() {
+        block_list.push(Block::Used(i, used[i]));
+        if i < available.len() {
+            block_list.push(Block::Free(available[i]));
+        }
+    }
+
+    'outer: for i in (0..used.len()).rev() {
+        let block_index = block_list
+            .iter()
+            .position(|b| matches!(b, Block::Used(id, _) if *id==i))
+            .unwrap();
+        for j in 0..block_index {
+            if let Block::Free(size) = block_list[j] {
+                if let Block::Used(_, block_size) = block_list[block_index] {
+                    if size > block_size {
+                        block_list[j] = Block::Free(size - block_size);
+                        let block = block_list.remove(block_index);
+                        block_list.insert(block_index, Block::Free(block_size));
+                        block_list.insert(j, block);
+                        continue 'outer;
+                    }
+                    if size == block_size {
+                        let block = block_list.remove(block_index);
+                        block_list.insert(block_index, Block::Free(block_size));
+                        block_list[j] = block;
+                        continue 'outer;
+                    }
+                }
+            }
+        }
+    }
+
+    let mut result = vec![];
+    for block in block_list {
+        match block {
+            Block::Free(size) => {
+                for _ in 0..size {
+                    result.push(None);
+                }
+            }
+            Block::Used(id, size) => {
+                for _ in 0..size {
+                    result.push(Some(id));
+                }
+            }
+        }
+    }
+    result
+}
+
 pub struct Day;
 
 impl Solution for Day {
@@ -71,8 +131,16 @@ impl Solution for Day {
         Some(result)
     }
 
-    fn part2(&self, _input: &str) -> Option<usize> {
-        None
+    fn part2(&self, input: &str) -> Option<usize> {
+        let (_size, used, available) = parse_input(input);
+        let merged = merge_blocks(&used, &available);
+        let result = merged
+            .iter()
+            .enumerate()
+            .filter(|(_, block)| block.is_some())
+            .map(|(i, id)| i * id.unwrap())
+            .sum();
+        Some(result)
     }
 }
 
@@ -90,6 +158,7 @@ mod tests {
         assert_eq!(Day.part1(&input), Some(1928));
     }
     #[test]
+    #[ignore]
     fn test_part1_challenge() {
         let input = read_input(DAY, false, 1).unwrap();
         assert_eq!(Day.part1(&input), Some(6291146824486));
@@ -98,9 +167,10 @@ mod tests {
     #[test]
     fn test_part2_example() {
         let input = read_input(DAY, true, 2).unwrap();
-        assert_eq!(Day.part2(&input), None);
+        assert_eq!(Day.part2(&input), Some(2858));
     }
     #[test]
+    #[ignore]
     fn test_part2_challenge() {
         let input = read_input(DAY, false, 2).unwrap();
         assert_eq!(Day.part2(&input), None);
