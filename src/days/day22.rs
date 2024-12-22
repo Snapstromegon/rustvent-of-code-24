9e86cbc6952c6e09c15849ad71f52ab746635c26
Raphael Höser <raphael@hoeser.dev>
2024-12-22 13:15:37 +0100

    Add day 22.2

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -1,3 +1,7 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
+use rayon::prelude::*;
+
 use crate::solution::{Solution, SolvedValue};
 
 fn mix_and_prune(current: usize, value: usize) -> usize {
@@ -12,6 +16,10 @@ fn next_secret(current: usize) -> usize {
     secret
 }
 
+fn get_price(secret: usize) -> usize {
+    secret % 10
+}
+
 fn parse_input(input: &str) -> Vec<usize> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
@@ -20,18 +28,44 @@ pub struct Day;
 
 impl Solution for Day {
     fn part1(&self, input: &str) -> Option<SolvedValue> {
-        let inputs = parse_input(input);
-
-        Some(inputs.into_iter().map(|mut secret| {
-            for _ in 0..2000 {
-                secret = next_secret(secret);
-            }
-            secret
-        }).sum::<usize>().into())
+        Some(
+            parse_input(input)
+                .into_par_iter()
+                .map(|secret| (0..2000).fold(secret, |secret, _| next_secret(secret)))
+                .sum::<usize>()
+                .into(),
+        )
     }
 
-    fn part2(&self, _input: &str) -> Option<SolvedValue> {
-        None
+    fn part2(&self, input: &str) -> Option<SolvedValue> {
+        let inputs = parse_input(input);
+
+        let mut global_diffs = HashMap::new();
+
+        inputs.into_iter().for_each(|mut secret| {
+            let mut last_price = get_price(secret);
+            let mut diffs = VecDeque::with_capacity(4);
+            let mut seen = HashSet::new();
+            for _ in 0..2000 {
+                secret = next_secret(secret);
+                let current_price = get_price(secret);
+                let price_diff = last_price as isize - current_price as isize;
+                last_price = current_price;
+                diffs.push_back(price_diff);
+                if diffs.len() > 3 {
+                    if !seen.contains(&diffs) {
+                        seen.insert(diffs.clone());
+                        global_diffs
+                            .entry(diffs.clone())
+                            .and_modify(|x| *x += current_price)
+                            .or_insert(current_price);
+                    }
+                    diffs.pop_front();
+                }
+            }
+        });
+
+        Some(global_diffs.values().max().unwrap().clone().into())
     }
 }
 
@@ -51,17 +85,18 @@ mod tests {
     #[test]
     fn test_part1_challenge() {
         let input = read_input(DAY, false, 1).unwrap();
-        assert_eq!(Day.part1(&input), None);
+        assert_eq!(Day.part1(&input), Some(15_613_157_363.into()));
     }
 
     #[test]
     fn test_part2_example() {
         let input = read_input(DAY, true, 2).unwrap();
-        assert_eq!(Day.part2(&input), None);
+        assert_eq!(Day.part2(&input), Some(23.into()));
     }
     #[test]
+    #[ignore = "takes too long"]
     fn test_part2_challenge() {
         let input = read_input(DAY, false, 2).unwrap();
-        assert_eq!(Day.part2(&input), None);
+        assert_eq!(Day.part2(&input), Some(1784.into()));
     }
 }
